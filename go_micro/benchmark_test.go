9f78075678bd7ff0111438cb9a4e093c4173ac22
santiweide <ggy_778@sina.com>
2021-05-24 16:48:47 +0800

    remove client

--- go_micro/benchmark_test.go
+++ go_micro/benchmark_test.go
@@ -1,6 +1,17 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/registry"
+	"github.com/micro/go-micro/registry/consul"
+	"log"
+	"test_go_micro"
+	"test_go_micro/go_micro/model"
+	"testing"
+	"unsafe"
+)
 
 func BenchmarkTestString(b *testing.B){
 	for n:= 0;n < b.N;n ++{
@@ -15,3 +26,67 @@ func BenchmarkTestStruct(b *testing.B){
 	}
 }
 
+
+
+func testString() {
+
+	reg := consul.NewRegistry(
+		func(options *registry.Options) {
+			options.Addrs = []string{
+				"139.198.174.188:8500",
+			}
+		})
+
+	// create a new service
+	service := micro.NewService(micro.Registry(reg))
+
+	// parse command line flags
+	service.Init()
+	greeter := model.NewGreeterService("Greeter", service.Client())
+	req := &model.StringRequest{
+		Message: test_go_micro.RandStringRunes(10000),
+	}
+
+	log.Printf("Request Size: %v\n", unsafe.Sizeof(req))
+	// Use the generated client stub
+	_, err := greeter.TestString(context.Background(), req)
+
+	if err == nil {
+		fmt.Printf("error:%v\n", err)
+	}
+}
+
+func testStruct() {
+
+	reg := consul.NewRegistry(
+		func(options *registry.Options) {
+			options.Addrs = []string{
+				"139.198.174.188:8500",
+			}
+		})
+
+	// create a new service
+	service := micro.NewService(micro.Registry(reg))
+
+	// parse command line flags
+	service.Init()
+	greeter := model.NewGreeterService("Greeter", service.Client())
+
+	_map := make(map[int32]string)
+	_list := make([]string,100)
+	for i := 0;i < 100;i ++ {
+		_map[int32(i)]=test_go_micro.RandStringRunes(100)
+		_list[i] = test_go_micro.RandStringRunes(100)
+	}
+	req := &model.StructRequest{
+		Id:         123,
+		KvMap:      _map,
+		StringList: _list,
+	}
+	log.Printf("Request Size: %v\n", unsafe.Sizeof(req))
+	// Use the generated client stub
+	_, err := greeter.TestStruct(context.Background(), req)
+	if err == nil {
+		fmt.Printf("error:%v\n", err)
+	}
+}
--- grpc/benchmark_test.go
+++ grpc/benchmark_test.go
@@ -1,18 +1,85 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"test_go_micro"
+	"test_go_micro/grpc/model"
+	"testing"
+	"unsafe"
+)
 
-func BenchmarkTestString(b *testing.B){
-	for n:= 0;n < b.N;n ++{
+const (
+	target = "consul://139.198.174.188:8500/test_grpc"
+)
+
+func BenchmarkTestString(b *testing.B) {
+	for n := 0; n < b.N; n++ {
 		testString()
 	}
 }
 
-
-func BenchmarkTestStruct(b *testing.B){
-	for n:= 0;n < b.N;n ++{
+func BenchmarkTestStruct(b *testing.B) {
+	for n := 0; n < b.N; n++ {
 		testStruct()
 	}
 }
 
+func testString() {
+	conn, err := grpc.Dial(
+		target,
+		//		"consul://127.0.0.1:8500/test_grpc",
+		grpc.WithInsecure(),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 
+	c := model.NewGreeterClient(conn)
+
+	req := &model.StringRequest{
+		Message: test_go_micro.RandStringRunes(10000),
+	}
+
+	log.Printf("Request Size: %v\n", unsafe.Sizeof(req))
+	_, err = c.TestString(context.Background(), req)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+}
+
+func testStruct() {
+	conn, err := grpc.Dial(
+		target,
+		//		"consul://127.0.0.1:8500/test_grpc",
+		grpc.WithInsecure(),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	c := model.NewGreeterClient(conn)
+
+	_map := make(map[int32]string)
+	_list := make([]string, 100)
+	for i := 0; i < 100; i++ {
+		_map[int32(i)] = test_go_micro.RandStringRunes(100)
+		_list[i] = test_go_micro.RandStringRunes(100)
+	}
+	req := &model.StructRequest{
+		Id:         123,
+		KvMap:      _map,
+		StringList: _list,
+	}
+	log.Printf("Request Size: %v\n", unsafe.Sizeof(req))
+
+	_, err = c.TestStruct(context.Background(), req)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+}
